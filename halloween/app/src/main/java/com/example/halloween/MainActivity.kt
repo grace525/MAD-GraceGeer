7491c5e3b447195f6cafe45c04a833f89520e9c5
grace525 <59849227+grace525@users.noreply.github.com>
2021-11-02 08:34:04 -0600

    Add files via upload

--- halloween/app/src/main/java/com/example/halloween/MainActivity.kt
+++ halloween/app/src/main/java/com/example/halloween/MainActivity.kt
@@ -14,11 +14,19 @@ class MainActivity : AppCompatActivity() {
 
     fun sayBoo(view: android.view.View) {
         val booText = findViewById<TextView>(R.id.textMessage)
+        val changeThis = findViewById<TextView>(R.id.newText)
+//        val booImage = findViewById<ImageView>(R.id.imageView)
         val editName = findViewById<EditText>(R.id.editTextName)
         val name = editName.text
         booText.text = "Happy Halloween " + name + "!"
+        changeThis.text = "You pressed the button!"
+        val imageSkull = findViewById<ImageView>(R.id.imageView)
+        imageSkull.setImageResource(R.drawable.skull)
+//        booImage.setBackgroundResource = (R.drawable.skull)
 
-        val imageGhost = findViewById<ImageView>(R.id.imageView)
-        imageGhost.setImageResource(R.drawable.ghost)
+
+
+//        val imageGhost = findViewById<ImageView>(R.id.imageView)
+//        imageGhost.setImageResource(R.drawable.ghost)
     }
 }
